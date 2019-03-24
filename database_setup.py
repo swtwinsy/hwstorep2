e6bf53510a462fbe6c98740b4a943fd93ad6de47
swtwinsy <lfscwn60@gmail.com>
2019-03-23 19:39:18 -0700

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -13,7 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    picture = Column(String(250))
+    picture = Column(String(5000))
 
 
 class Hardware_Category(Base):
